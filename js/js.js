dae32bee27f35ff2664d503724a360e54b7a7b19
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-02 20:52:30 +0200

    css correection

--- js/js.js
+++ js/js.js
@@ -1,32 +1,37 @@
-"use strict";
-
 // Hide Sections
 $('.section').hide();
+
 setTimeout(function () {
   $(document).ready(function () {
     // Show sections
-    $('.section').fadeIn(); // Hide preloader
+    $('.section').fadeIn();
 
-    $('.loader').fadeOut(); //Init Side nav
+    // Hide preloader
+    $('.loader').fadeOut();
 
-    $('.button-collapse').sideNav(); // Init Modal
+    //Init Side nav
+    $('.button-collapse').sideNav();
 
-    $('.modal').modal(); // Init Select
+    // Init Modal
+    $('.modal').modal();
 
-    $('select').material_select(); // Counter
+    // Init Select
+    $('select').material_select();
 
+    // Counter
     $('.count').each(function () {
       $(this).prop('Counter', 0).animate({
         Counter: $(this).text()
       }, {
-        duration: 1000,
-        easing: 'swing',
-        step: function step(now) {
-          $(this).text(Math.ceil(now));
-        }
-      });
-    }); // Comments - Approve & Deny
+          duration: 1000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+    });
 
+    // Comments - Approve & Deny
     $('.approve').click(function (e) {
       Materialize.toast('Comment Approved', 3000);
       e.preventDefault();
@@ -34,20 +39,34 @@ setTimeout(function () {
     $('.deny').click(function (e) {
       Materialize.toast('Comment Denied', 3000);
       e.preventDefault();
-    }); // Quick Todos
+    });
 
+    // Quick Todos
     $('#todo-form').submit(function (e) {
-      var output = "<li class=\"collection-item\">\n            <div>".concat($('#todo').val(), "\n              <a href=\"#!\" class=\"secondary-content delete\">\n                <i class=\"material-icons\">close</i>\n              </a>\n            </div>\n          </li>");
-      $('.todos').append(output);
-      Materialize.toast('Todo Added', 3000);
-      e.preventDefault();
-    }); // Delete Todos
+      const output = `<li class="collection-item">
+            <div>${$('#todo').val()}
+              <a href="#!" class="secondary-content delete">
+                <i class="material-icons">close</i>
+              </a>
+            </div>
+          </li>`;
 
+      $('.todos').append(output);
+
+      Materialize.toast('Todo Added', 3000);
+
+      e.preventDefault();
+    });
+
+    // Delete Todos
     $('.todos').on('click', '.delete', function (e) {
       $(this).parent().parent().remove();
       Materialize.toast('Todo Removed', 3000);
+
       e.preventDefault();
     });
+
     CKEDITOR.replace('body');
+
   });
 }, 1000);
