dfeecfc0f5d6b112188e2ffc454a2c1afda10ba8
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-02 17:06:09 +0200

    commit

--- projects/video-player-project/js/dist/scripts.dev.js
+++ projects/video-player-project/js/dist/scripts.dev.js
@@ -4,7 +4,7 @@ var video = document.getElementById('video');
 var play = document.getElementById('play');
 var stop = document.getElementById('stop');
 var progress = document.getElementById('progress');
-var timestamp = document.getElementById('timestamp');
+var timestamp = document.getElementById('timestamp'); // Play & pause video
 
 function toggleVideoStatus() {
   if (video.paused) {
@@ -12,7 +12,8 @@ function toggleVideoStatus() {
   } else {
     video.pause();
   }
-}
+} // update play/pause icon
+
 
 function updatePlayIcon() {
   if (video.paused) {
@@ -20,19 +21,44 @@ function updatePlayIcon() {
   } else {
     play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
-}
+} // Update progress & timestamp
+
 
 function updateProgress() {
-  return true;
-}
+  progress.value = video.currentTime / video.duration * 100; // Get minutes
+
+  var mins = Math.floor(video.currentTime / 60);
+
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  } // Get seconds
+
+
+  var secs = Math.floor(video.currentTime % 60);
+
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = "".concat(mins, ":").concat(secs);
+} // Set video time to progress
+
 
 function setVideoProgress() {
-  return true;
-}
+  video.currentTime = +progress.value * video.duration / 100;
+} // Stop video
+
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+} // Event listeners
+
 
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
 video.addEventListener('timeupdate', updateProgress);
 play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
 progress.addEventListener('change', setVideoProgress);
